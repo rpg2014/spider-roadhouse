5843340e9031bcc222acaaacfc6020e8ff79f4b5
rpg2014 <rpg2014@users.noreply.github.com>
2019-08-25 19:37:14 -0700

    Updated backend url

--- src/store/paths.ts
+++ src/store/paths.ts
@@ -1,4 +1,4 @@
-export const SPIDERMAN_BASE_URL = 'http://localhost:8080'; //'https://obscure-inlet-41841.herokuapp.com';
+export const SPIDERMAN_BASE_URL = 'https://obscure-inlet-41841.herokuapp.com';
 export const STATUS = "/server/status";
 export const DETAILS = "/server/details";
 export const START = "/server/start";
