ce16ce45f2bd0997e0f2095ac0e4c45929201baf
rpg2014 <rpg2014@users.noreply.github.com>
2019-08-25 20:45:01 -0700

    removed extra /

--- src/store/paths.ts
+++ src/store/paths.ts
@@ -1,4 +1,4 @@
-export const SPIDERMAN_BASE_URL = 'https://iron-spider.herokuapp.com/';
+export const SPIDERMAN_BASE_URL = 'https://iron-spider.herokuapp.com';
 export const STATUS = "/server/status";
 export const DETAILS = "/server/details";
 export const START = "/server/start";
