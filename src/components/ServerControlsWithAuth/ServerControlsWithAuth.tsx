d2be8f768b8ea97428cea48fd6569b384c96bf97
rpg2014 <pgiven14@gmail.com>
2019-08-28 21:13:45 -0700

    increased cookie storage time

--- src/components/ServerControlsWithAuth/ServerControlsWithAuth.tsx
+++ src/components/ServerControlsWithAuth/ServerControlsWithAuth.tsx
@@ -41,7 +41,7 @@ Amplify.configure({
         // OPTIONAL - Cookie path
             path: '/',
         // OPTIONAL - Cookie expiration in days
-            expires: 1, //365,
+            expires: 30, //365,
         // OPTIONAL - Cookie secure flag
         // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
             secure: true
