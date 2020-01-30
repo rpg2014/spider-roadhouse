1f7ec2d4d98c0beefa9b427b95f747d9a773c8e6
rpg2014 <rpg2014@users.noreply.github.com>
2020-01-29 23:34:40 -0800

    Think i fixed the token  refresh problem

--- src/components/Auth/common.ts
+++ src/components/Auth/common.ts
@@ -1,5 +1,5 @@
 import { CognitoUser } from "amazon-cognito-identity-js";
-import { useEffect } from "react";
+import { useEffect, Dispatch } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import IApplicationStore from "../../interfaces/IApplicationStore";
 import { registerAuthData, removeRefreshTimoutId, setRefreshTimoutId, setAccessToken } from "../../actions/authAction";
@@ -31,23 +31,34 @@ export const useAuthData = (authData?: CognitoUser) => {
 
   useEffect(() => {
     let id: NodeJS.Timeout;
-    // if there is not a saved authToken but there is auth data OR the saved AuthToken is different than the one in authData
-    if ((!authToken && authData) || (authToken && authData && authToken !== authData?.getSignInUserSession()?.getAccessToken().getJwtToken())) {
+    // if there is not a saved authToken but there is auth data
+    if (!authToken && authData) {
       dispatch(registerAuthData(authData));
       if (!isRefreshIdSet) {
-        id = setTimeout(() => {
-          console.log('Refreshing Session')
-          Auth.currentSession().then(data => {
-            console.log(data)
-            if(data.isValid()){
-              dispatch(setAccessToken(data.getAccessToken()))
-            }
-          })
-          dispatch(removeRefreshTimoutId());
-        }, 3600000); // 1 hour
-        // set Id in store
-        dispatch(setRefreshTimoutId(id));
+        setRefreshTimeout(dispatch)
+      }
+      // if authToken has been refreshed but the authData hasnt.  In this case authToken is up to date so set the refresh but not the authdata
+    } else if(authToken && authData && authToken !== authData?.getSignInUserSession()?.getAccessToken().getJwtToken()){
+      if(!isRefreshIdSet) {
+        setRefreshTimeout(dispatch)
       }
     }
   }, [authData, authToken, dispatch, isRefreshIdSet])
+}
+
+
+
+const setRefreshTimeout = (dispatch: Dispatch<any>) => {
+  let id = setTimeout(() => {
+    console.log('Refreshing Session')
+    Auth.currentSession().then(data => {
+      console.log(data)
+      if(data.isValid()){
+        dispatch(setAccessToken(data.getAccessToken()))
+      }
+    })
+    dispatch(removeRefreshTimoutId());
+  }, 3600000); // 1 hour
+  // set Id in store
+  dispatch(setRefreshTimoutId(id));
 }
